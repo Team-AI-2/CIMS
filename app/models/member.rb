085142623cd37ca2b5a3f77e1cb72ff13885051c
Ishaan Singh <asishaan.191cs124@nitk.edu.in>
2021-03-19 00:19:03 +0530

    Added position-member relation

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,7 +3,7 @@ class Member < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one :position
+  belongs_to :position
   has_many :requested_items, class_name: 'Order', foreign_key: 'requester_id'
   has_many :items_approved, class_name: 'Order', foreign_key: 'approver_id'
 end
