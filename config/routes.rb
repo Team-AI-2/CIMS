f98799e3780ac265deedd8b0996bc91a53d5cb4b
Ikjot Singh Dhody <ikjotsd@gmail.com>
2021-03-24 00:55:20 +0530

    routing fixed

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   devise_for :members
   get '/members/my_dashboard', to: 'members#my_dashboard'
   get '/members/new_member', to: 'members#new_member'
-  root to: 'pages#about'
+  get '/about', to: 'pages#about'
+  root to: 'members#my_dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
