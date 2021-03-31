23307a2493b15764a26754788f7882a0a5642032
Ishaan Singh <asishaan.191cs124@nitk.edu.in>
2021-03-31 09:03:06 +0530

    order workflow complete

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,11 +3,10 @@ class OrdersController < ApplicationController
   def check_login
     redirect_to member_session_path and return unless current_member
   end
-  before_action :set_order, only: %i[ show edit update destroy ]
+  before_action :set_order, only: %i[ edit_status update destroy ]
 
   # GET /orders or /orders.json
   def index
-   
     @club = current_member.club
     @order_history_approved = Order.where(requester_id: current_member.id,
                                           returned: false)
@@ -17,13 +16,12 @@ class OrdersController < ApplicationController
     @order_history_returned = Order.where(requester_id: current_member.id,
                                           returned: true)
                                    .where.not(approved_at: nil)
-    
-
   end
 
   
   # GET /orders/1 or /orders/1.json
   def show
+    redirect_to root_path, alert: "You are not authorised to view that page."
   end
 
   # GET /orders/new
@@ -33,15 +31,37 @@ class OrdersController < ApplicationController
 
   # GET /orders/1/edit
   def edit
+    redirect_to root_path, alert: "You are not authorised to view that page."
+  end
+
+  def edit_status
+    # redirect_to root_path, alert: "You are not authorised to view that page." unless ["Convenor"].include?(current_member.position.name)
+    if request.patch?
+      if params[:commit] == "Reject Request"
+        @order.update(approver: current_member, deadline: Time.now, returned: true)
+        redirect_to club_path(current_member.club), notice: "The Order Request was rejected successfully."
+      else
+        @order.update(approver: current_member, deadline: params[:deadline], returned: false, approved_at: Time.current)
+        redirect_to club_path(current_member.club), notice: "The Order Request was accepted successfully."
+      end
+    end
   end
 
   # POST /orders or /orders.json
   def create
-    @order = Order.new(quantity:params[:order][:quantity],item_id:params[:order][:item_id],requester: current_member, requested_at:Time.now, approver:Member.find(1),approved_at: nil,deadline: nil, returned:false)
+    @order = Order.new(
+      quantity:params[:order][:quantity],
+      item_id:params[:order][:item_id],
+      requester: current_member,
+      requested_at:Time.now,
+      approver:Member.find(1),
+      approved_at: nil,
+      deadline: nil,
+      returned:false)
 
     respond_to do |format|
       if @order.save
-        format.html { redirect_to @order, notice: "Order was successfully created." }
+        format.html { redirect_to orders_path, notice: "Order was successfully created." }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -52,13 +72,14 @@ class OrdersController < ApplicationController
 
   # PATCH/PUT /orders/1 or /orders/1.json
   def update
-    respond_to do |format|
-      if @order.update(order_params)
-        format.html { redirect_to @order, notice: "Order was successfully updated." }
-        format.json { render :show, status: :ok, location: @order }
+    # redirect_to root_path, alert: "You are not authorised to view that page." unless ["Convenor"].include?(current_member.position.name)
+    unless request.get?
+      if params[:commit] == "Reject Request"
+        @order.update(approver: current_member, deadline: Time.now, returned: true)
+        redirect_to club_path(current_member.club), notice: "The Order Request was rejected successfully."
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+        @order.update(approver: current_member, deadline: params[:order][:deadline], returned: false, approved_at: Time.current)
+        redirect_to club_path(current_member.club), notice: "The Order Request was accepted successfully."
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/members/my_dashboard', to: 'members#my_dashboard'
   get '/members/new_member', to: 'members#new_member'
   delete '/members/:id', to: 'members#destroy_member'
+  match '/orders/:id/edit_status', via: %w[get post patch], to: "orders#edit_status"
   get '/about', to: 'pages#about'
   get '/items/:id/orders', to: 'items#item_history'
   root to: 'members#my_dashboard'
